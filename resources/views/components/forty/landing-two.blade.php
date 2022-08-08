915c49de3aa1ca8f23b6866fd246a59464df20f3
Marco Hernandez <marcohern@gmail.com>
2022-08-07 19:19:30 -0500

    removing some links

--- resources/views/components/forty/landing-two.blade.php
+++ resources/views/components/forty/landing-two.blade.php
@@ -11,7 +11,7 @@
                 </header>
                 <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
                 <ul class="actions">
-                    <li><a href="generic.html" class="button">Learn more</a></li>
+                    <li><a href="#" class="button">Learn more</a></li>
                 </ul>
             </div>
         </div>
@@ -27,7 +27,7 @@
                 </header>
                 <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
                 <ul class="actions">
-                    <li><a href="generic.html" class="button">Learn more</a></li>
+                    <li><a href="#" class="button">Learn more</a></li>
                 </ul>
             </div>
         </div>
@@ -43,7 +43,7 @@
                 </header>
                 <p>Nullam et orci eu lorem consequat tincidunt vivamus et sagittis magna sed nunc rhoncus condimentum sem. In efficitur ligula tate urna. Maecenas massa sed magna lacinia magna pellentesque lorem ipsum dolor. Nullam et orci eu lorem consequat tincidunt. Vivamus et sagittis tempus.</p>
                 <ul class="actions">
-                    <li><a href="generic.html" class="button">Learn more</a></li>
+                    <li><a href="#" class="button">Learn more</a></li>
                 </ul>
             </div>
         </div>
