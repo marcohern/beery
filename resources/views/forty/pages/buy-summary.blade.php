236146fe4b625a2306ba7c9acb8613dc176e5f96
Marco Hernandez <marcohern@gmail.com>
2022-08-26 21:52:46 -0500

    added order summary header

--- resources/views/forty/pages/buy-summary.blade.php
+++ resources/views/forty/pages/buy-summary.blade.php
@@ -10,6 +10,22 @@
                 </header>
                 <div class="row">
                     <div class="col-8 col-12-md">
+                        <div class="table-wrapper">
+                            <table>
+                                <tr>
+                                    <td>Name</td><td>{{$order->name}}</td>
+                                <tr>
+                                <tr>
+                                    <td>Email</td><td>{{$order->email}}</td>
+                                <tr>
+                                <tr>
+                                    <td>Phone</td><td>{{$order->phone}}</td>
+                                <tr>
+                                <tr>
+                                    <td colspan="2">{{$order->comments}}</td>
+                                <tr>
+                            </table>
+                        </div>
                         <x-buy-summary :order="$order" :details="$details" :flavors="$flavors"/>
                     </div>
                     <div class="col-4 col-12-md">
