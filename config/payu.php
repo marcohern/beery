0113e6775ba0a84d94c081bbe57def5f73eede97
Marco Hernandez <marcohern@gmail.com>
2022-08-27 22:27:47 -0500

    added public key

--- config/payu.php
+++ config/payu.php
@@ -1,8 +1,9 @@
 <?php
 return [
-    'apiKey' => '4Vj8eK4rloUd272L48hsrarnUA',
-    'apiLogin' => 'pRRXKOl8ikMmt9u',
+    'apiKey'     => '4Vj8eK4rloUd272L48hsrarnUA',
+    'apiLogin'   => 'pRRXKOl8ikMmt9u',
+    'publicKey'  => 'PKaC6H4cEDJD919n705L544kSU',
     'merchantId' => '508029',
-    'currency' => 'COP',
-    'accountId' => '512321'
+    'currency'   => 'COP',
+    'accountId'  => '512321'
 ];
